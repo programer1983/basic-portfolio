f8e47c08ceeb8c004ea13305d17c3ef93f99e67a
programer1983 <programer972@gmail.com>
2023-09-17 15:20:18 +0300

    create burger menu

--- js/main.js
+++ js/main.js
@@ -1,1 +1,11 @@
 AOS.init()
+
+const headerburger = document.querySelector(".header__burgrer")
+const headerlist = document.querySelector(".header__list")
+
+headerburger.addEventListener("click", open)
+
+function open(){
+    headerburger.classList.toggle("active")
+    headerlist.classList.toggle("open-burger")
+}
