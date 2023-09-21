c2143d1ffc58774d9f2dcf9df864170c64a11acf
programer1983 <programer972@gmail.com>
2023-09-21 13:41:02 +0300

    script for smooth transition to website blocks by links in the menu

--- js/main.js
+++ js/main.js
@@ -2,6 +2,19 @@ AOS.init()
 
 const headerburger = document.querySelector(".header__burgrer")
 const headerlist = document.querySelector(".header__list")
+let html = document.querySelector("html")
+
+
+/* ======== Disabling site scrolling under the burger menu =================== */
+
+headerburger.addEventListener("click", burgerscroling)
+
+function burgerscroling(){
+    html.classList.toggle("unscroll")
+}
+
+
+/* ===== Opening burger menu ================================================ */
 
 headerburger.addEventListener("click", open)
 
@@ -11,19 +24,31 @@ function open(){
 }
 
 
+/* ======= Closing the burger menu after clicking on the link =============== */
+
 headerlist.querySelectorAll(".header__link").forEach(link => {
     link.addEventListener("click", () => {
+        headerburger.classList.remove("active")
         headerlist.classList.remove("open-burger")
+        html.classList.remove("unscroll")
     })
 })
 
 
-let html = document.querySelector("html")
-const burger = document.querySelector(".header__burgrer")
+/* ======== Smooth scrolling to site blocks from the menu =================== */
 
-burger.addEventListener("click", burgerscroling)
+const anchors = document.querySelectorAll('[href*="#"]');
 
-function burgerscroling(){
-    html.classList.toggle("unscroll")
-}
+anchors.forEach(anchor => {
+    anchor.addEventListener("click", (event) => {
+        event.preventDefault()
+
+        const blockID = anchor.getAttribute("href").substring(1)
+        document.getElementById(blockID).scrollIntoView({
+            behavior: "smooth",
+            block: "start",
+        })
+    })
+})
+
 
